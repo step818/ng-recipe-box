97343e207d345c99ded902c00ac78e84f5859305
Student <example@example.com>
2019-06-10 10:26:30 -0700

    initialized and added a list to our recipe box

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from './models/recipe.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'Baked goods only';
+  recipes: Recipe[] = [
+    new Recipe('Muffins', 'flour, sugar, baking powder, eggs, blueberries, milk, vanilla extract, veggie oil', 'Bake em til they are done'),
+    new Recipe('cookies!', 'more of the same but chocolate chips', 'The same as the muffins directions.')
+  ];
 }
